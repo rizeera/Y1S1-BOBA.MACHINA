12b93308e72f4179e67b471a31a0912709f57659
Caspeare <isadtaisa@gmail.com>
2019-11-19 20:43:55 +0700

    Update

--- PROJECT_BUBBLE_TEA.c
+++ PROJECT_BUBBLE_TEA.c
@@ -5,39 +5,39 @@
 char input[10];
 int input_temp;
 int tempS;
+int STRlength;
+int valid_check;
 
     
-/*
-    //  INPUT / OUTPUT //
-    char input[1] ;
-    int input_temp;
-    */ int TOPPING_amount; /*
+
+    /* INPUT / OUTPUT */ 
+    int TOPPING_amount; 
     
 
-    REFILLING THE MACHINE
+    /* REFILLING THE MACHINE */
     int TEA_price[6] = { 30,30,30,45,40,35 }; //base on Medium cup size's price
-    int FLAVOR_price[4] = { 0,5,5,10 }; */
+    int FLAVOR_price[4] = { 0,5,5,10 }; 
     int TOPPING_price[4] = { 5,15,10,10 }; 
     int TOPPING_unit[4] = { 2,0,0,0 };  //OR JUST FILL THE VALUE LATER;
-/*
-    MONEY ARRAY 
-    int MONEY_value[5] = { 100,50,20,10,5 };
-    int MONEY_machine[5] = { 0,0,0,0,0 };
-    int MONEY_insert[5];
+
+    /* MONEY ARRAY */
+     int MONEY_value[5] = { 100,50,20,10,5 };
+    int MONEY_machine[5] = { 0,4,0,1,2 }; 
+    int MONEY_insert[5]; 
     int MONEY_changes[5];
     
-    MONEY 
+    /* MONEY */
     int TOTAL_insert = 0;
     int TOTAL_change = 0;
-   */
-int TOTAL_price = 0; /*
+    int TOTAL_price = 0; 
+    int TOTAL_machine_money = 0;
 
-
+/*
                                                                                             /////
     int SELECTION_ARRAY[] = { (TEA_input) , (FLAVOR_input) , (TOPPING_1ST) , (TOPPING_2ND)};///// MAY NOT NEED IT
                                                                                             ///// COULD USE NEW FUNCTION TO PRINT SELECTED MENU //////////
                                                                                            
-    */
+*/
 
 int main()
 {
@@ -77,10 +77,10 @@ int main()
     printf("\n") ;
     printf("Toppings \n") ;
     printf("\n") ;
-    // printf("0:Black tea                          +5  (%d units left)  \n",top_unit[0] /*Put unit here naja*/) ;
-    // printf("1:Green tea                          +15 (%d units left)  \n",top_unit[1] /*Put unit here naja*/) ;
-    // printf("2:Thai tea                           +10 (%d units left)  \n",top_unit[2] /*Put unit here naja*/) ;
-    // printf("3:Passion fruit green tea            +10 (%d units left)  \n",top_unit[3] /*Put unit here naja*/) ;
+    printf("0:Black tea                          +5  (%d units left)  \n",TOPPING_unit[0] /*Put unit here naja*/) ;
+    printf("1:Green tea                          +15 (%d units left)  \n",TOPPING_unit[1] /*Put unit here naja*/) ;
+    printf("2:Thai tea                           +10 (%d units left)  \n",TOPPING_unit[2] /*Put unit here naja*/) ;
+    printf("3:Passion fruit green tea            +10 (%d units left)  \n",TOPPING_unit[3] /*Put unit here naja*/) ;
 
     ////////////////////////////////////
     ////////* ASK FOR BASSE TEA *///////
@@ -270,7 +270,129 @@ TOPPING_SCAN:
         }
 
     /* ///////////////////////////////////////////////////////////////// */
+
+    ////////////////////////////////////////////////
+    ///////// /* ARE YOU READY TO PAY ? */ /////////
+    ////////////////////////////////////////////////
     printf("Total price is %d", TOTAL_price);
+    printf("Are YOu ReaDY TO PaY nOw ? (y/n):");
+    scanf("%s", input);
+MONEY_SCAN:
+    if (input[1] != '\0') { input[0] = '\0'; goto RE_SCAN_MONEY; }
+    else
+    {
+        if (input[0] == 'n') { return 0; }
+        else if (input[0] != 'y')
+        {
+            goto RE_SCAN_MONEY;
+        }
+    }
+    RE_SCAN_MONEY:
+    while (input[0]!='n' && input[0]!='y' && input[1]!='\n')
+    {
+        printf("sorry, input again");
+        scanf("%s", input);
+        goto MONEY_SCAN;
+    }
+
+    ///////////////////////////////////////////////////////
+
+
+    //////////////////////////////////////////
+    ////// /* INSERT AMOUNT OF MONEY *////////
+    //////////////////////////////////////////
+    printf("Machine cash: ( ");
+    for (int i = 0; i < 5; i++)
+    {
+        if (i==4)   
+            printf("%d=%d) \n" ,MONEY_value[i] ,MONEY_machine[i]);
+        else  
+            printf("%d=%d,", MONEY_value[i], MONEY_machine[i]);
+    }
+
+        /* INSERT AMOUNT PART */
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d=:" ,MONEY_value[i]);
+        scanf("%s" ,input);
+
+CHECK_VALID:
+        STRlength = strlen(input);
+        for (int i = 0; i < STRlength; i++)
+        {
+            if (input[i] < '0' || input[i] > '9') { valid_check = 0; break;}
+            else { valid_check = 1; }
+        }
+
+        RE_SCAN_AMT:
+        if (valid_check==0)
+        {
+            printf("invalid, input again:");
+            scanf("%s" ,input);
+            goto CHECK_VALID;
+        }
+
+        input_temp = atoi(input);
+        MONEY_insert[i] = input_temp;
+        MONEY_machine[i] += input_temp;
+    }
+        
+    ///////////////////////////////////////////////
+    
+    printf("machine money:");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d " ,MONEY_machine[i]);
+    }
+    printf("\n");
+    printf("money insert :");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d " ,MONEY_insert[i]);
+    }
+    
+    
+
+    ///////////////////////////////////////////
+    ////////* CALCULATING FOR CHANGEs *////////
+    ///////////////////////////////////////////
+
+    for (int i = 0; i < 5; i++)
+    {
+        //TOTAL_insert += (MONEY_value[i]*MONEY_insert[i]); ใช้ทำบ้าอะไรฟระ 
+        TOTAL_machine_money += (MONEY_value[i]*MONEY_machine[i]);
+    }
+
+    if (TOTAL_price == TOTAL_machine_money)
+    {
+        /* CASE WHERE TOTAL_price equal to TOTAL SUM OF MONEY IN THE MACHINE */
+        for (int i = 0; i < 5; i++)
+        {
+            MONEY_changes[i] = MONEY_machine[i];
+            MONEY_machine[i] = 0;
+        }
+        
+    }
+    else if (TOTAL_price > TOTAL_machine_money)
+    {
+        /* CASE WHERE MACHINE DOESN"T HAVE ENOUGH MONEY */
+    }
+    else
+    {
+        /* NORMAL CASE */
+    }
+    
+    
+
+    
+    
+
+
+
+    
+
+
+    
 
     return 0;
 }
